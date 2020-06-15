1d76e9ff110be4953cf4499fd16a5c4208b03d00
alexregazzo <37711311+alexregazzo@users.noreply.github.com>
2020-06-15 14:16:46 -0300

    Update utils.py
    Relative dir problem

--- utils.py
+++ utils.py
@@ -1,6 +1,9 @@
 import json
+import os
 
 
 def get_data() -> dict:
-    with open("dados.json", encoding="utf8") as f:
+    with open(os.path.join(CURRENT_DIRPATH, "dados.json"), encoding="utf8") as f:
         return json.load(f)
+
+CURRENT_DIRPATH = os.path.dirname(__file__)
